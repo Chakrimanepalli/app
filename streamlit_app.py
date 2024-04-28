8bc91da8a8872a6514db17dde314e3b62a609c14
Chakravarthi Manepalli <137620571+Chakrimanepalli@users.noreply.github.com>
2024-04-28 12:24:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install xgboost
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from scipy import stats
